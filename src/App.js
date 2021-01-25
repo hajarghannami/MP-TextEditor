fa19be340eb76ab8fab63592a93cc0468f70c4e4
hajarghannami <hajarghannami@outlook.com>
2021-01-25 06:42:09 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 const styles = {
   bold: { fontWeight: "bold" },
@@ -11,14 +12,25 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 function App() {
+  const [text, setText] = useState("");
+  const [textColor, setTextColor] = useState("black");
+  const [textStyle, setTextStyle] = useState("");
+  console.log(text);
+
   const stylingBoxes = stylings.map((style) => (
-    <button className="btn btn-light" style={styles[style]} key={style}>
+    <button
+      onClick={() => setTextStyle(style)}
+      className="btn btn-light"
+      style={styles[style]}
+      key={style}
+    >
       {style}
     </button>
   ));
 
   const colorBoxes = colors.map((color) => (
     <button
+      onClick={() => setTextColor(color)}
       style={{ backgroundColor: color, height: 30, width: 30 }}
       key={color}
     />
@@ -27,7 +39,15 @@ function App() {
   return (
     <div className="App">
       <div className="my-3">{stylingBoxes}</div>
-      <textarea />
+      <textarea
+        style={{
+          color: textColor,
+          fontWeight: textStyle,
+          fontStyle: textStyle,
+          textDecorationLine: textStyle,
+        }}
+        onChange={(event) => setText(event.target.value)}
+      />
       <div className="my-3">{colorBoxes}</div>
     </div>
   );
